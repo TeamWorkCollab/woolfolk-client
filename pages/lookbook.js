59b39222a7f523b734ca7da56a28e4aa528f52a6
Elaine Alasagas <ealasagas@gmail.com>
2021-07-14 13:07:31 -0700

    adding pages

---
+++ pages/lookbook.js
@@ -0,0 +1,5 @@
+const LookBook = () => {
+    return <h1>LookBook page</h1>
+};
+
+export default LookBook;
---
+++ pages/story.js
@@ -0,0 +1,5 @@
+const Story = () => {
+    return <h1>Story page</h1>
+};
+
+export default Story;
